036b6fbcbbe3cf7623aeb47155c831997fd6eb3d
Angela <angelalamluu@gmail.com>
2020-12-08 17:59:03 -0800

    commit tree e07598ac922400f81c0f83201662151fa137d352

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,55 @@
 class Triangle
-  # write code here
+  attr_accessor :sides
+
+  @sides = []
+
+  def initialize(a, b, c)
+    @sides = [a, b, c]
+    @sides.sort!
+  end
+
+  def kind
+    if @sides.any?{|side| side <= 0} || @sides[0] + @sides[1] <= @sides[2]
+      raise TriangleError
+    elsif @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+# class Triangle
+#   attr_accessor :a, :b, :c
+  
+#   def initialize(a, b, c)
+#     @a = a
+#     @b = b
+#     @c = c
+#   end
+
+#   def kind
+#     if @a + @b < @c || @a + @c < @b || @b + @c = @a
+#     raise TriangleError
+#     elsif @a > 0 && @b > 0 && @c > 0
+#       if @a == @b && @b == @c
+#         :equilateral
+#       elsif @a != @b && @b != @c && @a != @c
+#         :scalene
+#       else
+#         :isosceles
+#       end
+
+#     else
+#       raise TriangleError
+#     end
+#   end
+
+#   class TriangleError < StandardError
+#   end
+# end
